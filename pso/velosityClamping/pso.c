c816ede06d16d971ed5bb0f36c69c0fd4050f7d4
AlejandroRG <garcia4312@hotmail.com>
2023-03-23 11:47:06 -0600

    nuevo metodo del algoritmo PSO

--- pso/velosityClamping/pso.c
+++ pso/velosityClamping/pso.c
@@ -19,18 +19,19 @@ typedef struct{ PARTICULA    *Part;
                 float C2;
                 float *LimSup;
                 float *LimInf;
+                float k; //factor de velocidad
                }ENJAMBRE;
 
 //PARAMETROS DE CONFIGURACION DEL PSO
-#define DIM_ 2
-const unsigned int NumeroParticulas = 5; //Tamaño del enjambre, numero de particulas del enjambre
+#define DIM_ 5
+const unsigned int NumeroParticulas = 60; //Tamaño del enjambre, numero de particulas del enjambre
 const unsigned int Dimension=DIM_; //Numero de variables del problema o dimension del problema.
-const float LimiteSuperior[DIM_] = { 20, 20};
-const float LimiteInferior[DIM_] = {-20, -20};
-const unsigned int NumeroMaximoDeIteraciones = 100;
+const float LimiteSuperior[DIM_] = { 20, 20, 20, 20, 20};
+const float LimiteInferior[DIM_] = {-20, -20, -20, -20, -20};
+const unsigned int NumeroMaximoDeIteraciones = 600;
 
 ENJAMBRE* CrearEnjambre(unsigned int NumPart,unsigned int NumParam);
-void InicializarEnjambre(ENJAMBRE *Enj, float C1, float C2, unsigned int NumIterMax,const float *LInf,const float *LSup);
+void InicializarEnjambre(ENJAMBRE *Enj, float k, float C1, float C2, unsigned int NumIterMax,const float *LInf, const float *LSup);
 void EliminarEnjambre(ENJAMBRE *Enj);
 void ImprimeParticula(ENJAMBRE *Enj, unsigned int Id);
 void ImprimeEnjambre(ENJAMBRE *Enj);
@@ -47,11 +48,11 @@ int main(){
     //Crear un enjambre de NumeroParticulas de Numero de parametros igual a Dimension
     Enj=CrearEnjambre(NumeroParticulas,Dimension);
     printf("\nSe creo el ENJAMBRE");
-    InicializarEnjambre(Enj,2.0,2.0,NumeroMaximoDeIteraciones,LimiteInferior,LimiteSuperior);
+    InicializarEnjambre(Enj,0.075,2.0,2.0,NumeroMaximoDeIteraciones,LimiteInferior,LimiteSuperior);
     EvaluacionInicialEnjambre(Enj);
     printf("\n\n=============\n Iteracones 0 \n=============");
     ImprimeEnjambre(Enj);
-    while (t<Enj->NumMaxIter)
+    while (t<Enj->NumMaxIter) <>
     {
         
         printf("\n\n=============\n Iteracones %i \n=============",t);
@@ -65,6 +66,8 @@ int main(){
 
     printf("\n\n|...... BEST PARTICLE ......|");
     ImprimeParticula(Enj, Enj->IdPg);
+    printf("\n|...........................|");
+
     EliminarEnjambre(Enj);
     printf("\nSe elimino el ENJAMBRE");
     return 0;
@@ -109,20 +112,31 @@ void ActualizarPosicion(ENJAMBRE *Enj){
 
 void ActualizarVelocidad(ENJAMBRE *Enj){
     float Y1, Y2;
+    float Vmin, Vmax;
     for(unsigned int i=0; i<Enj->NumPart; i++) //Para cada particula i
     for(unsigned int j=0; j<Enj->NumParam; j++) //Para cada parametro j de cada vector de la particula i
     { 
+        Vmax = Enj->k * (Enj->LimSup[j]-Enj->LimInf[j]) / 2;
+        Vmin = -Vmax; 
         Y1 = (float)rand()/RAND_MAX;
         Y2 = (float)rand()/RAND_MAX;
         //formula de la veloidad
         Enj->Part[i].Vi[j] += (Enj->C1*Y1*(Enj->Part[i].Pi[j] - Enj->Part[i].Xi[j]) + Enj->C2*Y2*(Enj->Part[Enj->IdPg].Pi[j] - Enj->Part[i].Xi[j]));  
+        if( Enj->Part[i].Vi[j] > Vmax ){
+            Enj->Part[i].Vi[j] = Vmax;
+        }
+        if( Enj->Part[i].Vi[j] < Vmin ){
+            Enj->Part[i].Vi[j] = Vmin;
+        }
     }
 }
 
 float FuncionObjetivo(float *Xi){
     float fit;
-    /*Maximisar la siguiente funcion f(x,y)=50-(x-5)^2-(y-5)^2*/
-    fit = 50 - pow(Xi[0]-5, 2) - pow(Xi[1]-5, 2);
+    /*Maximisar la siguiente funcion 
+    f(w,x,y,z)=250 -(w+4)^2-(x+1)^2 -(y-3)^2 -(x-5)^2    
+    */
+    fit = 250 - pow(Xi[0]+7, 2) - pow(Xi[1]+3, 2) - pow(Xi[1]-3, 2) - pow(Xi[1]-5,2)- pow(Xi[1]-8, 2);
     return fit;
 }
 
@@ -151,36 +165,39 @@ void ImprimeEnjambre(ENJAMBRE *Enj)
 }
 void ImprimeParticula(ENJAMBRE *Enj, unsigned int Id)
     { 
-        printf("\nXi: ");
+        printf("\n| Xi: ");
         for(unsigned int i=0; i<Enj->NumParam; i++)
             printf("%f, ",Enj->Part[Id].Xi[i]);
-        printf("\nVi: ");  
+        printf("\n| Vi: ");  
         for(unsigned int i=0; i<Enj->NumParam; i++)
             printf("%f, ",Enj->Part[Id].Vi[i]);
-        printf("\nPi: ");      
+        printf("\n| Pi: ");      
         for(unsigned int i=0; i<Enj->NumParam; i++)
             printf("%f, ",Enj->Part[Id].Pi[i]);
-        printf("\nXfit=%f",Enj->Part[Id].Xfit);
-        printf("\nPfit=%f",Enj->Part[Id].Pfit);
+        printf("\n| Xfit=%f",Enj->Part[Id].Xfit);
+        printf("\n| Pfit=%f",Enj->Part[Id].Pfit);
       }
 
-void InicializarEnjambre(ENJAMBRE *Enj, float C1, float C2, unsigned int NumIterMax,const float *LInf, const float *LSup)
-{ float aux,rango;
-  Enj->C1=C1;
-  Enj->C2=C2;
-  Enj->NumMaxIter=NumIterMax;
-  Enj->IdPg=0;
-  Enj->LimSup=LSup;
-  Enj->LimInf=LInf;
-  //Inicializar cada vector de cada particula
-  for(unsigned int i=0; i<Enj->NumPart; i++) //Para cada particula i
-  for(unsigned int j=0; j<Enj->NumParam; j++) //Para cada parametro j de cada vector de la particula i
-     { rango=Enj->LimSup[j]-Enj->LimInf[j];
-       aux= ((float)rand()/RAND_MAX) * rango + Enj->LimInf[j];
-       Enj->Part[i].Xi[j]=aux;
-       Enj->Part[i].Vi[j]=0;
-       Enj->Part[i].Pi[j]=aux;
-      }
+void InicializarEnjambre(ENJAMBRE *Enj, float k, float C1, float C2, unsigned int NumIterMax,const float *LInf, const float *LSup)
+{ 
+    float aux,rango;
+    Enj->k = k;
+    Enj->C1=C1;
+    Enj->C2=C2;
+    Enj->NumMaxIter=NumIterMax;
+    Enj->IdPg=0;
+    Enj->LimSup=LSup;
+    Enj->LimInf=LInf;
+    //Inicializar cada vector de cada particula
+    for(unsigned int i=0; i<Enj->NumPart; i++) //Para cada particula i
+    for(unsigned int j=0; j<Enj->NumParam; j++) //Para cada parametro j de cada vector de la particula i
+    { 
+        rango=Enj->LimSup[j]-Enj->LimInf[j];
+        aux= ((float)rand()/RAND_MAX) * rango + Enj->LimInf[j];
+        Enj->Part[i].Xi[j]=aux;
+        Enj->Part[i].Vi[j]=0;
+        Enj->Part[i].Pi[j]=aux;
+    }
 }
 
 void EliminarEnjambre(ENJAMBRE* Enj)
@@ -212,9 +229,11 @@ ENJAMBRE* CrearEnjambre( unsigned int NumPart,unsigned int NumParam){
     //Reservar la memoria para N particulas de M parametros
     ptr->Part=(PARTICULA *)malloc(NumPart*sizeof(PARTICULA));
     if(ptr->Part==NULL){
-        printf("Error al reservar la memoria para las Particulas.");
+        printf("\n|ERROR| al reservar la memoria para las Particulas.");
         exit(0);
-    } 
+    } else {
+        printf("\n|CORRECTO| Se reservo la memoria para las Particulas\n");
+    }
     //Reservar memoria para los 3 vectores de cada Particula
     for(unsigned int i=0; i<NumPart; i++){ 
         ptr->Part[i].Xi=(float *)malloc(NumParam*sizeof(float));
