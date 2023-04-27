b62ed11f09700e8d053a83cb5106ad12c7f7f62d
AlejandroRG <garcia4312@hotmail.com>
2023-04-27 12:02:39 -0600

    nwe functions in ags

--- algoritmoGenetico/ags.c
+++ algoritmoGenetico/ags.c
@@ -1,6 +1,7 @@
 //Algoritmo Genetico Simple
 #include<stdio.h>
 #include<stdlib.h>
+#include <math.h>
 
 #define NumeroDeGenes_ 3
 const unsigned int NumeroDeGenes = NumeroDeGenes_;
@@ -11,39 +12,84 @@ const float LimiteInferior[NumeroDeGenes_] = {-1, -1, -1};
 typedef unsigned char Byte;
 
 typedef struct{
-        Byte *Cromosoma;
-        unsigned int NdGens;
-        const unsigned int *NdBxGen;
-        unsigned int CromSize;
-        int *ValE;
-        float *ValR;
-        const float *LimSup;
-        const float *LimInf;
+        Byte                *Cromosoma;
+        unsigned int        NdGens;
+        const unsigned int  *NdBxGen;
+        unsigned int        CromSize;
+        int                 *ValE;
+        float               *ValR;
+        const float         *LimSup;
+        const float         *LimInf;
 }INDIVIDUO;
 
 INDIVIDUO* Inicializar();
 void DecodeEntero(INDIVIDUO *p);
-void MuestraIndividuo(INDIVIDUO *p);
+void ShowIndividuo(INDIVIDUO *p);
+void ShowDecodeEntero(INDIVIDUO *p);
+void DecodeReal(INDIVIDUO *p);
 
 int main(){
     INDIVIDUO *p;
 
     p = Inicializar();
-    MuestraIndividuo(p);
+    //DecodeEntero(p);
+    DecodeReal(p);
+    ShowIndividuo(p);
+    ShowDecodeEntero(p);
 
     return 0;
 }
 
 void DecodeEntero(INDIVIDUO *p){
-
+    int inicio = 0;
+    for(int i=0; i<p->NdGens; i++){
+        p->ValE[i]=0;
+        for(int k = inicio; k < (p->NdBxGen[i]+ inicio); k++){
+            p->ValE[i] = pow(2,k-inicio) * p->Cromosoma[k];
+        }
+        inicio += p->NdBxGen[i];
+    }
+        
 }
 
-void MuestraIndividuo(INDIVIDUO *p){
-    unsigned int k;
+void DecodeReal(INDIVIDUO *p){
+    float rango;
+    int i,k,Inicio =0;
 
+    for(i=0; i<p->NdGens; i++){
+        p->ValE[i] = 0;
+         for(int k = Inicio; k < (p->NdBxGen[i]+ Inicio); k++){
+            p->ValE[i] = pow(2,k-Inicio) * p->Cromosoma[k];
+        }
+        Inicio += p->NdBxGen[i];
+    }
+    for(i=0; i<p->NdGens; i++){
+        rango = p->LimSup[i] - p->LimInf[i];
+        p->ValR[i] = (p->ValE[i] / (pow(2, p->NdBxGen[i]) - 1)) * rango + p->LimInf[i];
+    }
+}
+
+void ShowDecodeEntero(INDIVIDUO *p){
+    int k;
+    printf(" - ");
+    for(k=p->NdGens-1; k>=0; k--){
+        printf("%i, ", p->ValE[k]);
+    }
+}
+
+void ShowIndividuo(INDIVIDUO *p){
+     int k, acumulador, j;
+    acumulador = p->CromSize - p->NdBxGen[p->NdGens - 1];
+    j = p->NdGens - 1;
     printf("\n");
-    for(k = 0; k < p->CromSize; k++)
-        printf("%d", p->Cromosoma[k]);
+    for(k = p->CromSize-1; k >= 0; k--){
+        if(k==acumulador - 1){
+            printf(":");
+            j--;
+            acumulador -= p->NdBxGen[j];
+        }
+        printf("%d ",p->Cromosoma[k]);
+    }
 }
 
 INDIVIDUO* Inicializar(){
